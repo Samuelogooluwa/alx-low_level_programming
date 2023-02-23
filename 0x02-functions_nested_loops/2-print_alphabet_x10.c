65b07a6eb1e6e4e3f8108a21b0064c9e2d4bc06e
“Samuelogooluwa” <samuelayomide889@gmail.com>
2023-02-23 08:14:51 -0800

    v

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,28 +1,23 @@
 #include "main.h"
 
-
-	/**
-	 * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
-	 * followed by a new line
-	 */
-	void print_alphabet_x10(void)
+/**
+ * print_alphabet_x10 - a function that prints 10 times the alphabet
+ *
+ * Return: x10 a-z
+ */
+void print_alphabet_x10(void)
 {
-		char ch;
-		int i;
+	int n, co;
 
+	co = 0;
 
-		i = 0;
-
-
-		while (i < 10)
+	while (co < 10)
 	{
-			ch = 'a';
-			while (ch <= 'z')
-			{
-				_putchar(ch);
-				ch++;
-			}
-			_putchar('\n');
-			i++;
+		for (n = 'a'; n <= 'z'; n++)
+		{
+			_putchar(n);
+		}
+		co++;
+		_putchar('\n');
 	}
 }
