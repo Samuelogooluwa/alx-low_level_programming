0603489cc3066bea1b66798f5eea7cc3d5c1f80f
“Samuelogooluwa” <samuelayomide889@gmail.comgit config --global user.email samuelayomide889@gmail.com>
2023-03-21 03:30:12 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -10,19 +10,17 @@
  */
 int _strlen(char *s)
 {
-	int i;
+int i;
 
-
-	i = 0;
+i = 0;
 
 
 while (s[i] != '\0')
-	{
-		i++;
+{
+i++;
 	}
 
-
-	return (i);
+return (i);
 }
 
 
@@ -37,25 +35,24 @@ while (s[i] != '\0')
  */
 char *_strcpy(char *dest, char *src)
 {
-	int len, i;
+int len, i;
 
 
-	len = 0;
+len = 0;
+
+while (src[len] != '\0')
+{
+len++;
+}
 
 
-	while (src[len] != '\0')
-	{
-		len++;
-	}
+for (i = 0; i < len; i++)
+{
+dest[i] = src[i];
+}
+dest[i] = '\0';
 
-
-	for (i = 0; i < len; i++)
-	{
-		dest[i] = src[i];
-	}
-	dest[i] = '\0';
-
-	return (dest);
+return (dest);
 }
 
 
@@ -69,36 +66,36 @@ char *_strcpy(char *dest, char *src)
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *dog;
-	int len1, len2;
+dog_t *dog;
+int len1, len2;
 
 
-	len1 = _strlen(name);
-	len2 = _strlen(owner);
+len1 = _strlen(name);
+len2 = _strlen(owner);
 
 
-	dog = malloc(sizeof(dog_t));
-	if (dog == NULL)
-	return (NULL);
+dog = malloc(sizeof(dog_t));
+if (dog == NULL)
+return (NULL);
 
+dog->name = malloc(sizeof(char) * (len1 + 1));
 
-	dog->name = malloc(sizeof(char) * (len1 + 1));
-	if (dog->name == NULL)
-	{
-		free(dog);
-		return (NULL);
-	}
-	dog->owner = malloc(sizeof(char) * (len2 + 1));
+if (dog->name == NULL)
+{
+free(dog);
+return (NULL);
+}
+dog->owner = malloc(sizeof(char) * (len2 + 1));
 if (dog->owner == NULL)
-	{
-		free(dog);
-		free(dog->name);
-		return (NULL);
-	}
-	_strcpy(dog->name, name);
-	_strcpy(dog->owner, owner);
-	dog->age = age;
+{
+free(dog);
+free(dog->name);
+return (NULL);
+}
+_strcpy(dog->name, name);
+_strcpy(dog->owner, owner);
+dog->age = age;
 
 
-	return (dog);
+return (dog);
 }
