d3fb6acb0251d9d443fe76a93e742abd4835a03d
“Samuelogooluwa” <samuelayomide889@gmail.comgit config --global user.email samuelayomide889@gmail.com>
2023-03-22 06:02:04 -0700

    3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -7,6 +7,7 @@ int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
 
+
 /**
  * op_add - Returns the sum of two numbers.
  * @a: The first number.
@@ -26,7 +27,7 @@ int op_add(int a, int b)
  * Return: The difference of a and b.
  */
 int op_sub(int a, int b)
-{	
+{
 	return (a - b);
 }
 /**
